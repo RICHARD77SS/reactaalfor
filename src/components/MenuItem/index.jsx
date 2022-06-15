8879f30133ad61861643d7ab8bd86573b5a70f8b
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-06-15 19:17:40 -0300

    Mudança de nomes De(listMenuItems) para(MenuItem)

---
+++ src/components/MenuItem/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ListItemStyle, UlMenuItemStyle } from './styles';
+
+const MenuItem = () => {
+  
+  return (
+    <UlMenuItemStyle >
+      <ListItemStyle><Link className='LinkRd' to='/'>Home</Link></ListItemStyle>
+      <ListItemStyle><Link className='LinkRd' to='/store'>Store</Link></ListItemStyle>
+      <ListItemStyle><Link className='LinkRd' to='/contact'>Contact</Link></ListItemStyle>
+      <ListItemStyle><Link className='LinkRd' to='/abault'>Abault</Link></ListItemStyle>
+    </UlMenuItemStyle>
+  );
+};
+
+export default MenuItem;
