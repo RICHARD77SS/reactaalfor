5be9d07ad3859f17d4d0038d24207b3b909cf796
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-06-23 17:35:18 -0300

    Componentes estilizados da pagina de produto

--- src/components/produtoPage/styles.jsx
+++ src/components/produtoPage/styles.jsx
@@ -4,17 +4,22 @@ export const ProdutoPageContainerSt = styled.div`
   width: 100%;
   height: auto;
   display: flex;
+  flex-direction: column;
 `
 export const ProdutoContentSt = styled.div`
   width: 100%;
   height: auto;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
 `
 export const ProdutoConteinerImageSt = styled.div`
-  width: 70%;
+  width: 40%;
   height: auto;
   display: flex;
+  margin: 0 1rem;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
   flex-direction: column;
 `
 export const ProdutoAddFavSt = styled.div`
@@ -25,37 +30,54 @@ export const ProdutoAddFavSt = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const ProdutoBuySt = styled.div`
-  width: 40%;
+  width: 30%;
+  padding: 2rem;
   height: auto;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: 0 1rem;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const ProdutoInfosSt = styled.div`
   width: 100%;
   height: auto;
   display: flex;
-  align-items: center;
+  align-items: top;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const ProdutoDescSt = styled.div`
-  width: 100%;
+  width: 60%;
   height: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const LojaInfoSt = styled.div`
-  width: 100%;
-  height: auto;
+  width: 50%;
+  height: 40%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const PerguntasSt = styled.div`
   width: 100%;
@@ -64,14 +86,20 @@ export const PerguntasSt = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const PerguntasRespostasSt = styled.div`
-  width: 100%;
+  width: 80%;
   height: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
 export const MaisProdutosSt = styled.div`
   width: 100%;
@@ -80,4 +108,51 @@ export const MaisProdutosSt = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
+`
+export const AvaliacoesSt = styled.div`
+  width: 40%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
+`
+export const AvaliacaoUsersInfoSt = styled.div`
+  width: 100%;
+  height: auto;
+  margin: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
+`
+export const UserSt = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
+`
+export const AvaliacaoUserSt = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: ${props => props.theme.colors.bordeer};
 `
